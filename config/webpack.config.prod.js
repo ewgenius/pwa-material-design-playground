b7d72bf6ea66019f2c5b216c498122da1a87a705
ewgenius <ewgeniux@gmail.com>
2016-08-29 14:22:20 +0300

    init

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
+const loaders = require('./loaders')
+const vendor = require('./vendor')
+
+module.exports = {
+  entry: {
+    app: [path.join(__dirname, '../src/app.tsx')],
+    vendor
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, '../build')
+  },
+  module: {
+    loaders
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js', Infinity),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ]
+}
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const gulp = require('gulp')
+const util = require('gulp-util')
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+
+const webpackConfigDev = require('./config/webpack.config.dev')
+const webpackConfigProd = require('./config/webpack.config.prod')
+
+gulp.task('copy', () => {
+  return gulp.src([
+    `./index.html`,
+    './manifest.json'
+    ])
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('assets', () => {
+  return gulp.src('./assets/**/*.png')
+    .pipe(gulp.dest(`./build/assets`))
+})
+
+gulp.task('client:compile', cb => {
+  webpack(webpackConfigProd, (err, stats) => {
+    if (err) throw new gutil.PluginError('webpack', err)
+    util.log('[webpack]', stats.toString())
+    cb()
+  })
+})
+
+gulp.task('webpack', cb => {
+  webpack(webpackConfigProd, (err, stats) => {
+    if (err) throw new gutil.PluginError('webpack', err)
+    gutil.log('[webpack]', stats.toString())
+    cb()
+  })
+})
+
+gulp.task('serve', () => {
+  const compiler = webpack(webpackConfigDev)
+
+  const server = new WebpackDevServer(compiler, {
+    contentBase: './build',
+    hot: true,
+    stats: {
+      colors: true
+    }
+  })
+
+  server.listen(8080, 'localhost', err => {
+    if (err) throw new gutil.PluginError('webpack-dev-server', err)
+    util.log('[webpack-dev-server]', 'http://localhost:8080')
+  })
+})
+
+gulp.task('default', ['copy', 'assets', 'serve'])
