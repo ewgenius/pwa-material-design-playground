a15978bf7f3ead773a8247ebddb7ad0a1afea82e
ewgenius <ewgeniux@gmail.com>
2016-08-31 10:49:26 +0300

    upd

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {render} from 'react-dom'
 import * as configure from 'react-tap-event-plugin'
-import {MuiThemeProvider, getMuiTheme} from 'material-ui/styles'
+import {MuiThemeProvider, getMuiTheme, colors} from 'material-ui/styles'
 import './styles/main.scss'
 
 import App from './components/App.tsx'
@@ -11,6 +11,14 @@ configure()
 
 const container = document.querySelector('#root')
 const theme = getMuiTheme({
+  palette: {
+    primary1Color: colors.amber400,
+    primary2Color: colors.amber400,
+    primary3Color: colors.amber400,
+    accent1Color: colors.deepOrangeA400,
+    accent2Color: colors.deepOrangeA400,
+    accent3Color: colors.deepOrangeA400
+  },
   appBar: {
     height: 56
   }
--- src/containers/views/LoginView.tsx
+++ src/containers/views/LoginView.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {Component} from 'react'
-import {AppBar} from 'material-ui'
+import {AppBar, FloatingActionButton} from 'material-ui'
+import ContentAdd from 'material-ui/svg-icons/content/add'
 import HeaderLayout from '../../components/Layout/HeaderLayout.tsx'
 
 export default class App extends Component<{}, {}> {
@@ -9,7 +10,15 @@ export default class App extends Component<{}, {}> {
       appBar={
         <AppBar title='login'/>
       }>
-      test
+      <FloatingActionButton
+        secondary={true}
+        style={{
+          position: 'fixed',
+          right: 16,
+          bottom: 16
+        }}>
+        <ContentAdd />
+      </FloatingActionButton>
     </HeaderLayout>
   }
 }
